78422b25db2856a61dd2d228b9dad75c6090a78e
Hamza El Meknassi <hamza.elmeknassi@deecision.com>
2023-12-08 15:44:13 +0100

    Add Telescope keymaps map

---
+++ lua/custom/init.lua
@@ -0,0 +1,17 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt.relativenumber = true
+  end,
+})
---
+++ lua/custom/mappings.lua
@@ -0,0 +1,25 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>cu"] = {
+      function()
+        require("treesitter-context").go_to_context()
+      end,
+      "jump to upward context",
+    },
+    ["<leader>df"] = { ":w !diff % -", "diff buffer vs disk", opts = { nowait = true } },
+    ["<leader>fk"] = { "<cmd> Telescope keymaps <CR>", "Find keymap" },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+    ["M"] = { ":m '<-2<CR>gv=gv", "move line up" },
+    ["m"] = { ":m '>+1<CR>gv=gv", "move line down" },
+  },
+}
+
+-- more keybinds!
+
+return M
